92aa1259ce359f378fc72ec0962ca5b284ff21c8
Meet Zaveri <meethpatel96@gmail.com>
2017-09-12 23:38:06 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,25 +2,25 @@ export default {
     pictures: [
       {
         'id': 0,
-        'url': 'https://25.media.tumblr.com/tumblr_m40h4ksiUa1qbyxr0o1_400.gif',
+        'url': 'https://i.pinimg.com/736x/1a/ce/f4/1acef44ecba3dd4095da84c69e1f88ca.jpg',
         'comment': 'A cat game',
         'info': 'Posted by Kevin on Friday'
       },
       {
         'id': 1,
-        'url': 'https://25.media.tumblr.com/tumblr_lhd7n9Qec01qgnva2o1_500.jpg',
+        'url': 'https://media.licdn.com/media-proxy/ext?w=800&h=800&hash=DsQYNMqXJDBqXROLfPzvl8f%2FvWk%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta9Er0Vinkhwfjw8177yE41y87UNCVordEGXyD3u0qYrdfyG6fpHZK-SpuV8eK38clAMwfvKhRGHpD8fqfY--fdRy3Z-zJo24ZxUBbFI8lWxI',
         'comment': 'Tatoo & cat',
         'info': 'Posted by Charles on Tuesday'
       },
       {
         'id': 2,
-        'url': 'https://24.media.tumblr.com/tumblr_m4j2atctRm1qejbiro1_1280.jpg',
+        'url': 'http://www.lacometanetwork.com/es/images/paginas/lacometa.com-soluciones-web.jpg',
         'comment': 'Santa cat',
         'info': 'Posted by Richard on Monday'
       },
       {
         'id': 3,
-        'url': 'https://25.media.tumblr.com/tumblr_m3rmbwhVB51qhwmnpo1_1280.jpg',
+        'url': 'http://www.pervo.ru/uploads/posts/2016-02/1455123402_razrabotka-saitov.jpg',
         'comment': 'Mexico cat',
         'info': 'Posted by Richard on Monday'
       },
